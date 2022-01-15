337653c9f2b43833060fee5f8fbad371be001818
Tizian <tizianappler@yahoo.de>
2022-01-15 16:45:51 +0100

    Cleaned up commented out code

--- frontend/src/pages/ListsPage.tsx
+++ frontend/src/pages/ListsPage.tsx
@@ -2,25 +2,12 @@ import {IListController, IList, IListSetter} from "../models/ShoppingItems";
 import {FormEventHandler, useEffect, useState} from "react";
 import Lists from "../components/Lists";
 import './ListsPage.scss'
-import axios from "axios";
-
-interface ITextInput {
-    textInput: { value: string }
-}
 
 export default function ListsPage(props: { controller: IListController }) {
     const {controller} = props
-
-    // const [lists, setLists] = useState( ()  => {
-    //     console.log("Lists")
-    //     return controller.getLists()
-    // })
-
     const [lists, setLists] = useState<IList[]>([])
 
-
     useEffect(() => {
-        // axios.get('http://localhost:5000/api/lists').then(response => setLists(response.data));
         controller.getLists(setLists)
     }, [])
 
@@ -33,10 +20,6 @@ export default function ListsPage(props: { controller: IListController }) {
         event.currentTarget.elements[0].value = ""
         console.log("Add-List: ", lists)
     }
-
-    /*const removeList = (listName: string) => {
-        controller.removeList(setLists, listName)
-    }*/
 
     const removeList = (setter: IListSetter) => (listName: string) => {
         controller.removeList(setter, listName)
