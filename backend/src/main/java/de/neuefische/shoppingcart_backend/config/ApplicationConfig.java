1880c3b7cdca04d4b93c98dfcde0ff5206d2c246
Tizian <tizianappler@yahoo.de>
2022-01-18 10:30:52 +0100

    Moved configs to config package

--- backend/src/main/java/de/neuefische/shoppingcart_backend/config/ApplicationConfig.java
+++ backend/src/main/java/de/neuefische/shoppingcart_backend/config/ApplicationConfig.java
@@ -1,9 +1,7 @@
-package de.neuefische.shoppingcart_backend;
+package de.neuefische.shoppingcart_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
