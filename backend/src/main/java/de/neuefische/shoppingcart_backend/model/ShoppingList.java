500a2621fce92d3c767adcc44640405c98ebd461
Tizian <tizianappler@yahoo.de>
2022-01-20 10:57:24 +0100

    Added userID to Shoppinglist, for connection to User

--- backend/src/main/java/de/neuefische/shoppingcart_backend/model/ShoppingList.java
+++ backend/src/main/java/de/neuefische/shoppingcart_backend/model/ShoppingList.java
@@ -4,8 +4,6 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -18,6 +16,7 @@ public class ShoppingList {
     @Id
     String id;
 
+    @NonNull String userID;
     @NonNull String listName;
     @NonNull List<Item> items;
 }
