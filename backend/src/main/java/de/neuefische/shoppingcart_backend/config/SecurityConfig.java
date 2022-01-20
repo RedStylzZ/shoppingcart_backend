5e000a703552f75070f7b775d570a9019a79fb4d
Tizian <tizianappler@yahoo.de>
2022-01-20 10:56:51 +0100

    Changed authentication methods and access rights

--- backend/src/main/java/de/neuefische/shoppingcart_backend/config/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/shoppingcart_backend/config/SecurityConfig.java
@@ -1,44 +1,54 @@
 package de.neuefische.shoppingcart_backend.config;
 
+import de.neuefische.shoppingcart_backend.filter.JwtAuthFilter;
 import de.neuefische.shoppingcart_backend.service.MongoUserDetailsService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    UserDetailsService mongoUsers;
+    JwtAuthFilter jwtAuthFilter;
+    UserDetailsService userDetailsService;
 
-    public SecurityConfig(MongoUserDetailsService mongoUsers) {
-        this.mongoUsers = mongoUsers;
+
+    public SecurityConfig(UserDetailsService userDetailsService, JwtAuthFilter jwtAuthFilter) {
+        this.userDetailsService = userDetailsService;
+        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(mongoUsers);
-        /*auth.inMemoryAuthentication()
-                .withUser("frank")
-                .password("frank1").roles("USER");*/
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/").authenticated()
-                .and().formLogin()
-                .and().httpBasic();
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/auth/**").permitAll()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
         return new Argon2PasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
