3b6010ebf8f1df2d9d3c739c89b6f73a73491607
Tizian <tizianappler@yahoo.de>
2022-01-21 10:43:21 +0100

    Changed API URL

--- frontend/src/controller/ItemAPIController.ts
+++ frontend/src/controller/ItemAPIController.ts
@@ -1,23 +1,23 @@
 import axios from "axios";
-import {apiURL, IItemController} from "../models/ShoppingItems";
+import {IItemController} from "../models/ShoppingItems";
 
 export default function ItemAPIController(): IItemController {
     return {
         getItems: (listName) => {
-            return axios.get(`${apiURL}/items/${listName}`)
+            return axios.get(`/api/items/${listName}`)
                 .then(response => response.data).catch(console.error)
         },
         addItem: (listName, newItem, quantity) => {
-            return axios.put(`${apiURL}/items/${listName}`, {itemName: newItem, itemCount: quantity})
+            return axios.put(`/api/items/${listName}`, {itemName: newItem, itemCount: quantity})
                 .then(response => response.data).catch(console.error)
         },
         removeItem: (listName, itemID, wholeItem) => {
             console.log("ID:", itemID)
-            return axios.delete(`${apiURL}/items/${listName}?itemID=${itemID}&wholeItem=${wholeItem}`)
+            return axios.delete(`/api/items/${listName}?itemID=${itemID}&wholeItem=${wholeItem}`)
                 .then(response => response.data).catch(console.error)
         },
         changeItem: (listName, itemID, newName) => {
-            return axios.post(`${apiURL}/items/${listName}?itemID=${itemID}&newName=${newName}`)
+            return axios.post(`/api/items/${listName}?itemID=${itemID}&newName=${newName}`)
                 .then(response => response.data).catch(console.error)
         }
     };
