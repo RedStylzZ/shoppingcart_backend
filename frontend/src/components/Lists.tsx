656b1369adb1b35f35c43d6ca0e51c3961ebbb90
Tizian <tizianappler@yahoo.de>
2022-01-24 14:28:18 +0100

    Cleanup

--- frontend/src/components/Lists.tsx
+++ frontend/src/components/Lists.tsx
@@ -2,20 +2,6 @@ import {IList, removeListFunc} from "../models/ShoppingItems";
 import ListCard from "./ListCard";
 
 
-/*const mapToCard = (listsPromise: IList[], removeList: removeListFunc) => {
-    const lists: IList[] = listsPromise
-    console.log("mapToCard:", lists)
-    return (
-        lists.map((list, index) => <ListCard list={list} remove={removeList} key={index}/>)
-        /!*Object.entries(lists)
-            .map((list, index) => {
-                    // console.log("Lists: ", list[1])
-                    return <ListCard list={list[1]} remove={removeList} key={index}/>
-                }
-            )*!/
-    );
-}*/
-
 export default function Lists(props: { lists: IList[], removeList: removeListFunc}) {
     if (props.lists == null && !Array.isArray(props.lists)) return null;
     return (
