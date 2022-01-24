b13b777ebc42f1f9568a8bbb6f2a3ff9b86ff5f6
Tizian <tizianappler@yahoo.de>
2022-01-24 11:04:18 +0100

    Added AuthRequired.tsx context for login check

---
+++ frontend/src/context/AuthRequired.tsx
@@ -0,0 +1,30 @@
+import {createContext, ReactElement, useState} from "react";
+import jwt_decode from 'jwt-decode'
+
+export interface IAuthContext {
+    token?: string,
+    jwtDecoded?: {sub?: string },
+    setJwt: (jwt: string) => void
+}
+
+export const AuthContext = createContext<IAuthContext>({
+    setJwt: () => {throw "Login not initialized"}
+})
+
+export default function AuthRequired({children}: {children: ReactElement<any, any>}) {
+
+    const [token, setToken] = useState<string>()
+    const [jwtDecoded, setJwtDecoded] = useState({})
+
+    const setJwt = (jwt: string) => {
+        console.log(jwt)
+        setToken(jwt)
+        setJwtDecoded(jwt_decode(jwt.toString()))
+    }
+
+    return (
+        <AuthContext.Provider value={{token, jwtDecoded, setJwt}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
