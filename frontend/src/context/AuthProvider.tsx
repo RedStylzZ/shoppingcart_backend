5ff9627fe189590d6ad16bc27ba45d75979b0346
Tizian <tizianappler@yahoo.de>
2022-01-24 13:02:03 +0100

    Set config on token change

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,20 +1,30 @@
-import {createContext, ReactElement, useState} from "react";
+import React, {createContext, ReactElement, useEffect, useState} from "react";
 import jwt_decode from 'jwt-decode'
+import {TOKEN_KEY} from "../models/ShoppingItems";
 
 export interface IAuthContext {
     token?: string,
-    jwtDecoded?: {sub?: string },
+    jwtDecoded?: { sub?: string },
     setJwt: (jwt: string) => void
 }
 
 export const AuthContext = createContext<IAuthContext>({
-    setJwt: () => {throw "Login not initialized"}
+    setJwt: () => {
+        throw "Login not initialized"
+    }
 })
 
-export default function AuthProvider({children}: {children: ReactElement<any, any>}) {
+export default function AuthProvider({children, setConfig}: { children: ReactElement<any, any>, setConfig: React.Dispatch<React.SetStateAction<any>>}) {
 
-    const [token, setToken] = useState<string>()
+    const [token, setToken] = useState<string>(localStorage.getItem(TOKEN_KEY) || "")
     const [jwtDecoded, setJwtDecoded] = useState({})
+
+    useEffect(() => {
+        if (token != "") {
+            localStorage.setItem(TOKEN_KEY, token)
+        }
+        setConfig({headers: {Authorization: `Bearer ${token}`}})
+    }, [token])
 
     const setJwt = (jwt: string) => {
         console.log(jwt)
