1cb43476954a0c29d238b991cb300e8930920ed5
Tizian <tizianappler@yahoo.de>
2022-01-24 11:33:16 +0100

    Changed login procedure

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,17 +1,23 @@
-import {ILoginController, ITokenSetter} from "../models/ShoppingItems";
-import React, {FormEvent} from "react";
+import {ILoginController,} from "../models/ShoppingItems";
+import React, {ChangeEvent, FormEvent, useContext, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {AuthContext} from "../context/AuthProvider";
 
-export default function LoginPage(props: {controller: ILoginController, setter: ITokenSetter}) {
-    const {controller, setter} = props
+interface ITextInput {
+    username: { value: string }
+    password: { value: string }
+}
+
+export default function LoginPage(props: { controller: ILoginController }) {
+    const {controller} = props
     const navigate = useNavigate()
 
-    interface ITextInput {
-        username: { value: string }
-        password: { value: string }
-    }
+    const [name, setName] = useState<string>()
+    const [password, setPassword] = useState<string>()
 
-    const login: React.FormEventHandler<HTMLFormElement> = (event: FormEvent<HTMLFormElement>) => {
+    const {setJwt} = useContext(AuthContext)
+
+    const login = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         const form = event.currentTarget
         const formElements = form.elements as typeof form.elements & ITextInput
@@ -19,19 +25,25 @@ export default function LoginPage(props: {controller: ILoginController, setter: 
         const password: string = formElements.password.value.trim()
 
         if ((username && username.length > 0) && (password && password.length > 0)) {
-            controller.login(username, password).then(token => {
-                setter(token)
-                navigate("/lists")
-            })
+            controller.login(username, password)
+                .then(token => {
+                    setJwt(token)
+                    navigate("/lists")
+                })
+                .catch(console.error)
         }
     }
+
+    const onNameChange = (event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)
+    const onPasswordChange = (event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)
+
 
     return (
         <div className={"ChangePage"}>
             <h1>{"Login"}</h1>
             <form onSubmit={login}>
-                <input type="text" id={"username"}/>
-                <input type="password" id={"password"}/>
+                <input type="text" id={"username"} onChange={onNameChange} value={name}/>
+                <input type="password" id={"password"} onChange={onPasswordChange} value={password}/>
                 <input type="submit" value={"Submit"}/>
             </form>
         </div>
