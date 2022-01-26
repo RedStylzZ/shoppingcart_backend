a4e31cfea546e171d028389fd27e10aace9269db
Tizian <tizianappler@yahoo.de>
2022-01-26 12:26:48 +0100

    Cleanup and added log to login

--- backend/src/main/java/de/neuefische/shoppingcart_backend/controller/LoginController.java
+++ backend/src/main/java/de/neuefische/shoppingcart_backend/controller/LoginController.java
@@ -3,16 +3,13 @@ package de.neuefische.shoppingcart_backend.controller;
 import de.neuefische.shoppingcart_backend.model.MongoUser;
 import de.neuefische.shoppingcart_backend.model.dto.MongoUserDTO;
 import de.neuefische.shoppingcart_backend.service.LoginService;
-import de.neuefische.shoppingcart_backend.service.MongoUserDetailsService;
 import de.neuefische.shoppingcart_backend.service.UserService;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
-import java.util.Collection;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth/login")
@@ -24,25 +21,13 @@ public class LoginController {
         this.service = service;
     }
 
-    @GetMapping
-    public String helloWorld(Principal principal) {
-
-        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
-        LOG.info("Authorities: " + authorities);
-
-        if (authorities.stream()
-                .anyMatch(g -> MongoUserDetailsService.ROLE_ADMIN.equals(g.getAuthority()))) {
-            return "Hallo " + principal.getName();
-        }
-        return "Mach dich weg du Geringverdiener!";
-    }
-
     @PostMapping
     public String login(@RequestBody MongoUserDTO user) {
         MongoUser mongoUser = MongoUser.builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
                 .build();
+        LOG.trace("Logging in user: " + mongoUser.getPassword());
         return service.login(mongoUser);
     }
 
